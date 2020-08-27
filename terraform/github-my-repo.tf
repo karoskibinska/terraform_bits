7bd43f78cf156af09eb93cc2de34757610bebe32
Karo Skibinska <karolina.skibinska@makeitright.ai>
2020-08-27 12:33:33 +0200

    Add some faulty output

--- terraform/github-my-repo.tf
+++ terraform/github-my-repo.tf
@@ -9,3 +9,7 @@ module "github-repo-my-second-repo" {
   name        = "second-repo-name"
   description = ""
 }
+
+output "first-repo-full-name" {
+  value = "${module.github-repo-my-repo.full_name}"
+}
