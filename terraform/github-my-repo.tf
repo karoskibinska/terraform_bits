d1e17fa8602756736404aa00648c91614c02188a
Karo Skibinska <karolina.skibinska@makeitright.ai>
2020-08-27 12:23:00 +0200

    Add repo

--- terraform/github-my-repo.tf
+++ terraform/github-my-repo.tf
@@ -3,3 +3,9 @@ module "github-repo-my-repo" {
   name        = "repo-name"
   description = ""
 }
+
+module "github-repo-my-second-repo" {
+  source      = "./modules/github-repo"
+  name        = "second-repo-name"
+  description = ""
+}
